c2f41959fe5b9af5baf8c0894c9efc02ca7b7ca0
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-05-14 12:36:28 +0600

    added firebase config

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link, Links } from 'react-router'
 const Navbar = () => {
   return (
     <div>
-      <div className="container mx-auto mb-5">
-        <div className="flex justify-between py-3 border-b-2 ">
+      <div className="container mx-auto">
+        <div className="flex justify-between py-3 border-b-2 mb-5 ">
           <div className="">
             <h1>Navbar</h1>
           </div>
---
+++ src/pages/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeqkXu9ZdXRfrv0u0lqaB-41uMFFepseY",
+  authDomain: "crud-90edf.firebaseapp.com",
+  projectId: "crud-90edf",
+  storageBucket: "crud-90edf.firebasestorage.app",
+  messagingSenderId: "1030542276378",
+  appId: "1:1030542276378:web:9cba351df9cbebcacc1ea3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
